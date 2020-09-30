c7680f88bff7b554e0730c68466e5e25013c9317
Ramil <ramil.ming@gmail.com>
2020-09-30 13:06:22 -0700

    Start learning Javascript

---
+++ assignment/week_5/firstjavascript.js
@@ -0,0 +1,26 @@
+// JavaScript Console.
+console.log("Hello World");
+console.log("How are you doing?");
+console.log("I " + "Love" + " You");
+console.log("'I' " + "Love" + " You");
+
+// This is a single line comment
+
+/* 
+
+Multi-line comment
+
+*/
+
+// DATATYPES
+// String
+"This is a really long sentence. !@#$%%(()_+";
+
+// Number
+100;
+10.9;
+.70009;
+
+// Boolean
+true;
+false;
